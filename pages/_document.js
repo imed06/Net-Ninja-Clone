8eb24a3319e8b06bcc6f0780855820285c023efd
imed06 <78923328+imed06@users.noreply.github.com>
2023-06-28 04:45:20 -1100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head 
-        <meta name="google-site-verification" content="her4_vbrXV7GRznX_k7-tufMuc67rpYDbVCQngtqBiY" />
-      </Head>
+      <Head />
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <body>
         <Main />
